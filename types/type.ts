b8fa048736004e8816b56fc2d9ff5d2cf7c585d8
Aardvark <highcrypto098@gmail.com>
2025-05-20 04:10:57 -0700

    feat: add imf params interface

--- types/type.ts
+++ types/type.ts
@@ -3,3 +3,10 @@ export interface ApiResponse<T> {
   data: T;
   error?: string;
 }
+
+//IMF interface
+export interface IMFParams {
+  indicators: string[];
+  countries: string[];
+  years: string[];
+}
