03c6e36f9b063c09d98a327c890ac87596446595
Aardvark <highcrypto098@gmail.com>
2025-05-20 04:39:39 -0700

    feat: define initial setting

---
+++ app.ts
@@ -0,0 +1,17 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
+
+import { ImfGenerator } from "./utils/imfdata";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const pineconeApiKey = process.env.PINECONE_API_KEY;
+const openaiApikey = process.env.OPENAI_API_KEY;
+if (!pineconeApiKey || !openaiApikey) {
+  throw new Error("API key was not defined!");
+}
